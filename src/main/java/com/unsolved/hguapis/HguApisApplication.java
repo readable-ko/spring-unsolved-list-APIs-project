c3869b6060e60e8904f06a33e3584f821344e747
readable-ko <21700026@handong.edu>
2024-07-19 14:34:27 +0900

    style(typo): fix the wrong capital letter class name -HguApIsApplication -> HguApisApplication

--- src/main/java/com/unsolved/hguapis/HguApisApplication.java
+++ src/main/java/com/unsolved/hguapis/HguApisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HguApIsApplication {
+public class HguApisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HguApIsApplication.class, args);
+        SpringApplication.run(HguApisApplication.class, args);
     }
 
 }
