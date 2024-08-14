39aa715eb4dd5f008633948cd34dd58e478e5752
readable-ko <21700026@handong.edu>
2024-08-14 13:49:16 +0900

    refactor(mapper): fix wrong mapper bugs
    개선된 점:
    - 티어와 클래스가 반대로 매핑되고 있던 것을 변경

--- src/main/java/com/unsolved/hgu/userinfo/UserInfoMapper.java
+++ src/main/java/com/unsolved/hgu/userinfo/UserInfoMapper.java
@@ -4,9 +4,9 @@ public class UserInfoMapper {
     public static UserInfoDto ContributeDtoToDto(UserInfoContributeDto userInfoContributeDto) {
         return new UserInfoDto(
                 userInfoContributeDto.getUsername(),
-                userInfoContributeDto.getSubClass(),
-                Math.toIntExact(userInfoContributeDto.getSolvedCount()),
                 userInfoContributeDto.getTier(),
+                Math.toIntExact(userInfoContributeDto.getSolvedCount()),
+                userInfoContributeDto.getSubClass(),
                 userInfoContributeDto.getModifyDate()
         );
     }
